626f6b2749d8636decd64beb7e8b62d4a660f7be
Manish P <livinell73@gmail.com>
2025-03-09 22:52:24 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'database.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.urandom(24)
-BCRYPT_LOG_ROUNDS = 12
