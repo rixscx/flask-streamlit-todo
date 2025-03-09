5b981881d917d4f25c26edb76b671da6e6985fe4
Manish P <livinell73@gmail.com>
2025-03-09 23:31:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<div class="main-title">Enhanced To-Do List</div>', unsafe_allow_html=True)
+st.markdown('<div class="main-title">To-Do List</div>', unsafe_allow_html=True)
 
 # Function to get tasks
 def get_tasks():
